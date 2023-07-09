6a20c8a80012bddc33ecd687103e5a83d2e0a488
Ian <ian.farre@mia-platform.eu>
2023-07-09 17:19:48 +0200

    progress

--- backend/webview.go
+++ backend/webview.go
@@ -34,16 +34,55 @@ func main() {
 
 	// Commands
 	w.Bind("scan", func(filePath string, dateCol string, startTimeCol string, endTimeCol string) ([]string, error) {
-		f, err := excelize.OpenFile("your-file.xlsx")
+		f, err := excelize.OpenFile(filePath)
 		if err != nil {
 			message := fmt.Sprintf("Error opening the file: %s", err)
 			fmt.Println(message)
 			return nil, errors.New(message)
 		}
 
+		dateColIndex, err := excelize.ColumnNameToNumber(dateCol)
+		if err != nil {
+			message := fmt.Sprintf("Error getting the column for the date: %s", err)
+			fmt.Println(message)
+			return nil, errors.New(message)
+		}
+
+		startTimeColIndex, err := excelize.ColumnNameToNumber(startTimeCol)
+		if err != nil {
+			message := fmt.Sprintf("Error getting the column for the start time: %s", err)
+			fmt.Println(message)
+			return nil, errors.New(message)
+		}
+
+		endTimeColIndex, err := excelize.ColumnNameToNumber(endTimeCol)
+		if err != nil {
+			message := fmt.Sprintf("Error getting the column for the end time: %s", err)
+			fmt.Println(message)
+			return nil, errors.New(message)
+		}
+
 		sheetList := f.GetSheetList()
 		for _, sheetName := range sheetList {
-			fmt.Println(sheetName)
+			rows, err := f.GetRows(sheetName)
+			if err != nil {
+				message := fmt.Sprintf("Unexpected error, cannot read rows from file: %s", err)
+				fmt.Println(message)
+				return nil, errors.New(message)
+			}
+
+			// Delete first element as they are the titles
+			rows = rows[1:]
+
+			for _, row := range rows {
+				date := row[dateColIndex-1]
+				startTime := row[startTimeColIndex-1]
+				endTime := row[endTimeColIndex-1]
+
+				fmt.Println("date", date)
+				fmt.Println("startTime", startTime)
+				fmt.Println("endTime", endTime)
+			}
 		}
 
 		return sheetList, nil
